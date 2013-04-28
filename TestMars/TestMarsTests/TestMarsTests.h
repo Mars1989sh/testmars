4019f3d275824dfa1ab7ad5ffbe52f36f0bf8fad
张 炜超 <zhangweichao@zhangdeMac-mini.local>
2013-04-28 14:22:12 +0800

    创建项目

---
+++ TestMars/TestMarsTests/TestMarsTests.h
@@ -0,0 +1,13 @@
+//
+//  TestMarsTests.h
+//  TestMarsTests
+//
+//  Created by 张 炜超 on 13-4-28.
+//  Copyright (c) 2013年 张 炜超. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestMarsTests : SenTestCase
+
+@end
---
+++ TestMars/TestMarsTests/TestMarsTests.m
@@ -0,0 +1,32 @@
+//
+//  TestMarsTests.m
+//  TestMarsTests
+//
+//  Created by 张 炜超 on 13-4-28.
+//  Copyright (c) 2013年 张 炜超. All rights reserved.
+//
+
+#import "TestMarsTests.h"
+
+@implementation TestMarsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestMarsTests");
+}
+
+@end
